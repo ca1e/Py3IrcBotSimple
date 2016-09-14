04c25d37f1b3f40238c85439bf63bedbf0c6cf33
kizure <lucifr,kin@gmail.com>
2016-09-14 13:08:20 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Py3IrcSimpBot
+
+[这篇文章](https://stevearzh.github.io/2014/12/20/ircbot-1/)代码的py3版本
+
+感谢*stevearzh*
---
+++ sipircbot.py
@@ -0,0 +1,27 @@
+# --coding-- utf-8
+
+import socket
+
+irc_host = "irc.freenode.net"
+irc_port = 6667
+irc_chan = "#linuxba"
+
+bot_name = "mekubot"
+usr_name = "meku_bot"
+ral_name = "MekuTeleBot"
+
+irc_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+irc_sock.connect((irc_host, irc_port))
+
+
+irc_sock.send(("NICK " + bot_name + "\r\n").encode())
+irc_sock.send(("USER " + bot_name + " " + bot_name + " " + usr_name + " :" + ral_name + "\r\n").encode())
+irc_sock.send(("JOIN " + irc_chan + "\r\n").encode())
+
+while True:
+	data = irc_sock.makefile(encoding='utf-8')
+	for line in data:
+		print(line)
+		if(line.startswith("PING")):
+			send_back = line.replace("PING", "PONG")
+			irc_sock.send(send_back.encode())
